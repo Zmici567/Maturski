e09654adc63c06b053c34dc90fe5779d2c6808a9
Zmici567 <zmilica.567@gmail.com>
2023-04-24 20:24:39 +0200

    initial commit

---
+++ FRONT/lekcije/lekcije.js
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link href="boot/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    
+    <title>Ekosvest</title>
+
+    <link href="../style.css" rel="stylesheet">
+    <link href="stilelog.css" rel="stylesheet">
+</head>
+<body>
+    <!----------------------------------- ======= Navbar ======= -------------------------------------------->
+    <div class="nav">
+      <input type="checkbox" id="nav-check">
+      <div class="nav-header">
+        <div class="nav-title">
+          <img src="../slike/g109.png" class="nav-slika" style="height: 60px;" />
+        </div>
+      </div>
+
+      <div class="nav-btn">
+        <label for="nav-check">
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+      </div>
+      
+      <div class="nav-links">
+        <a class="navbar activ" href="pocetna.html" >Početna</a>
+        <a class="navbar" href="#onama" >O nama</a>
+        <a class="navbar" href="https://in.linkedin.com/in/jonesvinothjoseph">Lekcije</a>
+        <a class="navbar" href="https://codepen.io/jo_Geek/" >Prijavi se</a>
+      </div>
+    </div>
+</body>
+</html>
