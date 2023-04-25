6553663ba578aefb7e13fd550d52822535abe8dc
matija2004 <matijacetrovic@gmail.com>
2023-04-25 10:10:21 +0200

    povezan login

---
+++ FRONT/login/log.js
@@ -0,0 +1,30 @@
+const LINK = "http://localhost/";
+
+async function prijaviSE()
+{
+    let email = document.getElementById("cirko").value;
+    let password = document.getElementById("sojic").value;
+
+    let res = await axios.post(LINK+"api/login",{
+        email:email,
+        password:password
+    })
+    if(res.data.uspesnost)
+    {
+        console.log(res.data.id);
+        localStorage.setItem("id",res.data.id);
+        location.href="/";
+    }
+    else
+    {
+        console.log(res.data.message);
+        if(res.data.message === "404 not found")
+        {
+            document.getElementById("tika spic").innerHTML="Pogresan Email/Ime i Prezime ili Password";
+        }
+        else
+        {
+            document.getElementById("tika spic").innerHTML=res.data.message;
+        }
+    }
+}
