f87846e2aadee08975017d22120798004107546b
matija2004 <matijacetrovic@gmail.com>
2023-04-25 08:53:18 +0200

    innitial back

--- BACK/FUNCTIONS/odeljenje.js
+++ BACK/FUNCTIONS/odeljenje.js
@@ -5,9 +5,7 @@ async function get(req,res)
     try
     {
         let id = req.params.id;
-        let odeljenja = await ODELJENJE.find((idProfesora)=>{
-            return idProfesora===id;
-        });
+        let odeljenja = await ODELJENJE.find({idProfesora:id});
         res.json({
             uspesnost:true,
             odeljenja:odeljenja
@@ -22,6 +20,73 @@ async function get(req,res)
     }
 }
 
+async function post(req,res)
+{
+    try
+    {
+        let odeljenje = new ODELJENJE({
+            naziv:req.body.naziv,
+            skola:req.body.skola,
+            idProfesora:req.body.idProfesora,
+            idUcenika:[]
+        })
+        let saved = await odeljenje.save();
+        res.json({
+            uspesnost:true,
+            saved:saved
+        })
+    }
+    catch(err)
+    {
+        res.json({
+            uspesnost:false,
+            message:err.message
+        })
+    }
+}
+
+async function del(req,res)
+{
+    try
+    {
+        await ODELJENJE.deleteOne({_id:req.params.id})
+        res.json({uspesnost:true})
+    }
+    catch(err)
+    {
+        res.json({
+            uspesnost:false,
+            message:err.message
+        })
+    }
+}
+
+async function addUcenik(req,res)
+{
+    try
+    {
+        let id = req.params.id;
+        let idUcenika = req.body.idUcenika;
+        let odeljenje = await ODELJENJE.findById(id);
+        odeljenje.idUcenika.push(idUcenika);
+        let updated = await odeljenje.save();
+        res.json({
+            uspesnost:true,
+            updated:updated
+        })
+    }
+    catch(err)
+    {
+        res.json({
+            uspesnost:false,
+            message:err.message
+        })
+    }
+}
+
 module.exports = new Object({
-    get:get
+    get:get,
+    post:post,
+    delete:del,
+    addUcenik:addUcenik
 })
