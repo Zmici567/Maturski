9b9e0ee1b5233cd4bcdc3eb358b845f4eaf03ea7
matija2004 <matijacetrovic@gmail.com>
2023-05-15 17:08:45 +0200

    conflict

--- FRONT/reg/reg.js
+++ FRONT/reg/reg.js
@@ -4,23 +4,76 @@ async function registrujSE()
     try
     {
 
-        if((String)(document.getElementById("passwordProf").value)===(String)(document.getElementById("passwordProfPot").value))
+        let ImeIPrezime = document.getElementById("IiP").value;
+        let email = document.getElementById("email").value;
+        let pass = document.getElementById("pass").value;
+        let cpass = document.getElementById("cpass").value;
+
+        if(ImeIPrezime==="")
         {
-            let Profesor={
-                imeIprezime:(String)(document.getElementById("ImePrezimeProf").value),
-                email:(String)(document.getElementById("emailProf").value),
-                password:(String)(document.getElementById("passwordProf").value)
-            }
-            console.log(1);
-            var res=await axios.post(LINK + "/api/Profesor",Profesor);
-            console.log(res)    
-            let id=res.data.sacuvano._id;
-            console.log(id);
-            localStorage.setItem("id",id);
+            document.getElementById("tika spic").innerHTML="Niste uneli Ime i Pezime!!!"
+        }
+        else if(email==="")
+        {
+            document.getElementById("tika spic").innerHTML="Niste uneli Email!!!"
+        }
+        else if(pass === "")
+        {
+            document.getElementById("tika spic").innerHTML="Niste uneli sifru!!!"
+        }
+        else if(pass!==cpass)
+        {
+            document.getElementById("tika spic").innerHTML="Sifre se ne poklapaju!!!";
+        }
+        else if(!email.includes('@'))
+        {
+            document.getElementById("tika spic").innerHTML="Email nije validan!!!";
+        }
+        else if(pass.length<8)
+        {
+            document.getElementById("tika spic").innerHTML="Sifra ima manje od 8 karaktera!!!";
         }
         else
         {
-            console.log("nisu jednaki")
+            let t=false;
+            let u=false;
+            let v=false;
+            for(let i=0;i<pass.length;i++) 
+            {
+                let element=pass.charCodeAt(i);
+                if(element>=97 && element<=122)
+                {
+                    t=true;
+                }
+                else if(element>=65 && element<=90)
+                {
+                    u=true;
+                }
+                else if(element>=48 && element<=57)
+                {
+                    v=true;
+                }
+            }
+            if(!t || !u || !v)
+            {
+                document.getElementById("tika spic").innerHTML="Sifra nije dovoljno sigurna";
+            }
+            else
+            {
+                let res = (await axios.post(LINK+"api/Profesor",{
+                    imeIprezime:ImeIPrezime,
+                    password:pass,
+                    email:email
+                })).data;
+                if(res.uspesnost)
+                {
+                    location.href="/";
+                }
+                else
+                {
+                    document.getElementById("tika spic").innerHTML=res.message;
+                }
+            }
         }
        
     }
